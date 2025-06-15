05044476f0a214a3f8eda2ba77cbc0dc17276d0e
Lotar <amaral.lucas.lotar#gmail.com>
2025-06-15 07:16:20 -0300

    feat: add eslint-plugin-prettier and eslint-config-prettier to enhance linting and formatting

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,13 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default tseslint.config(
   { ignores: ['dist', 'README.md', 'node_modules'] },
+  { plugins: {
+      prettier: eslintPluginPrettier, // 👈 plugin registrado aqui
+  }},
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
